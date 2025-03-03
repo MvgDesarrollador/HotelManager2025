94c9c409c7febc573d885cbb97775b0da39904e1
GLOBANT\moises.villada <MOISES.VILLADA.GUEVARA.-ND@DISNEY.COM>
2025-03-03 12:00:16 -0500

    It was changed the main banner of the main page

--- src/components/common/Banner/Banner.jsx
+++ src/components/common/Banner/Banner.jsx
@@ -1,18 +1,15 @@
-import './Banner.css'
-export function Banner(){
-
-    return(
-
-        <>
-            <section className="banner">
-                <h2 className='text-white'>
-                    <span className="display-1">GestorApp</span>
-                    Aplicación para gestión de espacios en tu lugar
-                    de descanso...
-                </h2>
-
-            </section>
-        </>
-    )
-
+import "./Banner.css";
+export function Banner() {
+  return (
+    <>
+      <section className="banner">
+        <h2 className="text-white">
+          <span className="display-1">Hotel Manager</span>
+          <br />
+          Control and comfort in one only place, manage your hotel and
+          reservations here!
+        </h2>
+      </section>
+    </>
+  );
 }
