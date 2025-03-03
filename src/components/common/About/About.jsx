c06ff3e548cd74f891a346fe7d930508be1c6141
GLOBANT\moises.villada <MOISES.VILLADA.GUEVARA.-ND@DISNEY.COM>
2025-03-03 18:45:19 -0500

    It was redesigned the about section using the hotel mascots

--- src/components/common/About/About.jsx
+++ src/components/common/About/About.jsx
@@ -1,22 +1,32 @@
+import "./About.css";
+
 export function About() {
   return (
-    <section className="container my-5">
+    <section className="container my-5" id="mascots">
       <section className="row">
-        <section className="col-12 col-md-6">
+        <section className="col-12 col-md-3" id="mascot1">
           <img
-            src="../../../../src/assets/img/mascota.webp"
+            src="../../../../src/assets/img/03_03_2025_X-Design.png"
             alt="imagen"
-            className="img-fluid"
+            className="mascot-img"
           />
         </section>
-        <section className="col-12 col-md-6">
-          <h2>GestorApp</h2>
+        <section className="col-12 col-md-3" id="mascots-center">
+          <h2>Hotel Manager</h2>
           <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima
-            ipsa corporis cum accusantium, fuga ab eveniet reiciendis in culpa
-            expedita necessitatibus nihil quaerat accusamus ullam aperiam illo
-            adipisci sequi porro.
+            Discover the ultimate convenience in hotel booking with Hotel
+            Manager! Our user-friendly app allows you to effortlessly browse,
+            compare, and secure the best accommodations tailored to your needs.
+            Enjoy instant confirmations, all at your fingertips. Hire Hotel
+            Manager and enjoy taking your hotel to the next level!
           </p>
+        </section>
+        <section className="col-12 col-md-3" id="mascot2">
+          <img
+            src="../../../../src/assets/img/03_03_2025_X-Design (1).png"
+            alt="imagen"
+            className="mascot-img"
+          />
         </section>
       </section>
     </section>
