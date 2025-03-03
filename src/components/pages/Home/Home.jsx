dd64f3c166977621e261b9f4ff409f3bc130a27b
GLOBANT\moises.villada <MOISES.VILLADA.GUEVARA.-ND@DISNEY.COM>
2025-03-02 19:39:12 -0500

    It was created the footer common for the main page

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import { Banner } from "../../common/Banner/Banner";
 import { About } from "../../common/About/About";
+import { Footer } from "../../common/Footer/Footer";
 
 export function Home() {
   return (
     <>
       <Banner></Banner>
       <About></About>
+      <Footer></Footer>
     </>
   );
 }
