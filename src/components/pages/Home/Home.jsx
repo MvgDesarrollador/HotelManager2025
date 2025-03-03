8e6d3ba5f6018319679f81fe96380a4f0658a525
GLOBANT\moises.villada <MOISES.VILLADA.GUEVARA.-ND@DISNEY.COM>
2025-03-02 19:27:21 -0500

    It's created the main page and routes

---
+++ src/components/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import { Banner } from "../../common/Banner/Banner";
+import { About } from "../../common/About/About";
+
+export function Home() {
+  return (
+    <>
+      <Banner></Banner>
+      <About></About>
+    </>
+  );
+}
