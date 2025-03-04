cee34eb4e57116e8b316f377e84e206b407baaaa
GLOBANT\moises.villada <MOISES.VILLADA.GUEVARA.-ND@DISNEY.COM>
2025-03-03 19:29:32 -0500

    The reservations dashboard was redesigned

--- src/components/pages/Reservations/Reservations.jsx
+++ src/components/pages/Reservations/Reservations.jsx
@@ -9,7 +9,7 @@ export const Reservations = () => {
   const [guestName, setGuestName] = useState("");
 
   useEffect(() => {
-    // Load reservations from local storage
+    // Cargar reservas del almacenamiento local
     const storedReservations =
       JSON.parse(localStorage.getItem("reservations")) || [];
     setReservations(storedReservations);
@@ -23,7 +23,7 @@ export const Reservations = () => {
   }, []);
 
   const handleSpotClick = (index) => {
-    if (spots[index]) return; // Spot is already reserved
+    if (spots[index]) return; // Spot ya está reservado
     setSelectedSpot(index);
     setShowPopup(true);
   };
@@ -55,56 +55,70 @@ export const Reservations = () => {
   };
 
   return (
-    <div className="container">
-      <h2>Reservation Rooms</h2>
+    <section className="container my-5" id="reservations">
       <div className="row">
-        {spots.map((spot, index) => (
-          <div
-            key={index}
-            className={`spot ${spot ? "reserved" : "available"}`}
-            onClick={() => handleSpotClick(index)}
-          >
-            {index + 1}
+        {/* Columna de Reservaciones */}
+        <div className="col-12 col-md-6">
+          <h2>Reservation Rooms</h2>
+          <div className="row">
+            {spots.map((spot, index) => (
+              <div
+                key={index}
+                className={`spot ${spot ? "reserved" : "available"}`}
+                onClick={() => handleSpotClick(index)}
+              >
+                {index + 1}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
 
-      {showPopup && (
-        <div className="popup">
-          <h3>Enter Guest Name</h3>
-          <input
-            type="text"
-            value={guestName}
-            onChange={(e) => setGuestName(e.target.value)}
-            placeholder="Guest Name"
-          />
-          <button onClick={handleReservation}>Done</button>
-          <button onClick={() => setShowPopup(false)}>Cancel</button>
+          {showPopup && (
+            <div className="popup">
+              <h3>Enter Guest Name</h3>
+              <input
+                type="text"
+                value={guestName}
+                onChange={(e) => setGuestName(e.target.value)}
+                placeholder="Guest Name"
+              />
+              <button onClick={handleReservation}>Done</button>
+              <button onClick={() => setShowPopup(false)}>Cancel</button>
+            </div>
+          )}
+
+          <h2>Reservations</h2>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Room</th>
+                <th>Date</th>
+                <th>Hour</th>
+              </tr>
+            </thead>
+            <tbody>
+              {reservations.map((reservation, index) => (
+                <tr key={index}>
+                  <td>{reservation.name}</td>
+                  <td>{reservation.spot + 1}</td>
+                  <td>{reservation.date}</td>
+                  <td>{reservation.hour}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <button onClick={clearReservations}>Clear Reservations</button>
         </div>
-      )}
 
-      <h2>Reservations</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Room</th>
-            <th>Date</th>
-            <th>Hour</th>
-          </tr>
-        </thead>
-        <tbody>
-          {reservations.map((reservation, index) => (
-            <tr key={index}>
-              <td>{reservation.name}</td>
-              <td>{reservation.spot + 1}</td>
-              <td>{reservation.date}</td>
-              <td>{reservation.hour}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <button onClick={clearReservations}>Clear Reservations</button>
-    </div>
+        {/* Columna de Mascota */}
+        <div className="col-12 col-md-6 d-flex justify-content-center align-items-center">
+          <img
+            src="../../../../src/assets/img/murray.webp"
+            alt="imagen"
+            className="mascot-img-especial"
+          />
+        </div>
+      </div>
+    </section>
   );
 };
